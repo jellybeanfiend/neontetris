7b0e402e07fa8dacd5a1ea008bd7125876ab3b1a
Sevena Skeels <tookieblade@gmail.com>
2013-08-14 21:26:19 -0400

    added shapes

--- main.js
+++ main.js
@@ -19,6 +19,7 @@
 	})
 
 	function gameloop(time){
+
 		var start = Date.now()
 
 		update_logic(time)
@@ -33,14 +34,48 @@
 			return
 	}
 
-	var state = {
-		grid: []
-	}
-
 	var constants = {
 		game_area_width: canvas.width - 200,
 		cells_across : 10,
 		cells_vertically: 20
+	}
+
+	var state = {
+		grid: [],
+		current_shape: {},
+		currentx: (constants.game_area_width / 2) - (constants.game_area_width / constants.cells_across),
+		currenty: 0
+	}
+
+	var shapes = {
+		square : {
+			shape : [['yellow', 'yellow'],['yellow', 'yellow']],
+			color : 'yellow'
+		},
+		lowercasel : {
+			shape : [['aqua'],['aqua'],['aqua'],['aqua']],
+			color: 'aqua'
+		},
+		z : {
+			shape : [['transparent','green','green'],['green', 'green','transparent']],
+			color: 'green'
+		},
+		backwardsz : {
+			shape : [['red','red','transparent'],['transparent','red','red']],
+			color : 'red'
+		},
+		upperl : {
+			shape : [['orange','transparent'],['orange','transparent'],['orange','orange']],
+			color : 'orange'
+		},
+		backwardsupperl : {
+			shape : [['transparent','blue'], ['transparent','blue'],['blue','blue']],
+			color: 'blue'
+		},
+		prong : {
+			shape: [['transparent','purple','transparent'],['purple','purple','purple']],
+			color: 'purple'
+		}
 	}
 
 	function init(){
@@ -51,18 +86,32 @@
 			}
 		}
 		constants.cell_dim = constants.game_area_width / constants.cells_across
+		state.current_shape = shapes.lowercasel
 	}
 
 	function update_logic(time){
 		
+		if(state.currenty + (constants.cell_dim * 2) > canvas.height)
+			return
+		state.currenty += 1
+			
 	}
 
 	function update_screen(ctx){
+
+		ctx.fillStyle = "#444"
+		ctx.fillRect(0,0,constants.game_area_width,canvas.height)
 		
 		for(var x = 0; x < state.grid.length; x++){
 			for(var y = 0; y < state.grid[x].length; y++){
 				ctx.fillStyle = state.grid[x][y]
 				ctx.fillRect(x * constants.cell_dim, y * constants.cell_dim, constants.cell_dim - 1, constants.cell_dim - 1)
+			}
+		}
+		for(var x = 0; x < state.current_shape.shape.length; x++){
+			for(var y = 0; y < state.current_shape.shape[x].length; y++){
+				ctx.fillStyle = state.current_shape.shape[x][y]
+				ctx.fillRect((x * constants.cell_dim) + state.currentx, (y * constants.cell_dim) + state.currenty, constants.cell_dim - 1, constants.cell_dim - 1)
 			}
 		}
 	}
